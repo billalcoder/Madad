eb655fbff25b50dba898c85ed90ae9662a0d766c
billalcoder <billalshekhani10@gmail.com>
2025-11-06 17:40:09 +0530

    update api

--- utils/PushNotification.js
+++ utils/PushNotification.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
 const ONESIGNAL_APP_ID = "19446a03-690a-4ff1-a3b4-9aeb313da54e";
-const REST_API_KEY = "os_v2_app_dfcgua3jbjh7di5utlvtcpnfjzjbs554e5runjutblw2r7qrb4dpjzqs3ek7ayijglyt2du2c3mrqmqvf4jweovxceljcklr3cyyjgi"; // found in OneSignal dashboard
+const REST_API_KEY = "os_v2_app_dfcgua3jbjh7di5utlvtcpnfjzbwjkycpf4ed3ekympkzxpdqaylcbbx5jov2uy7hrcw5jkba4aynbkff2q7leejl5obgob6sexhcty"; // found in OneSignal dashboard
 
 export async function sendNotification(playerId, message) {
   const response = await fetch("https://onesignal.com/api/v1/notifications", {
@@ -12,8 +12,8 @@ export async function sendNotification(playerId, message) {
     },
     body: JSON.stringify({
       app_id: ONESIGNAL_APP_ID,
-      include_player_ids: [playerId],
-      contents: { en: message },
+      include_player_ids: ["84d77227-c5c6-479a-bba9-94175f93462b"],
+      contents: { en: "message" },
       headings: { en: "New Notification" },
     }),
   });
@@ -22,3 +22,4 @@ export async function sendNotification(playerId, message) {
   console.log("Notification Response:", data);
 }
 
+sendNotification()
